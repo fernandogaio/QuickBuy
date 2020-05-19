fce77c16170700751079d88cd9b90a3bd5397dff
Fernando Gaio <fernando_gaio@outlook.com>
2020-05-18 21:59:45 -0300

    Andamento

--- QuickBuy.Web/ClientApp/src/app/autorizacao/guarda-rotas.ts
+++ QuickBuy.Web/ClientApp/src/app/autorizacao/guarda-rotas.ts
@@ -1,22 +1,21 @@
 import { Injectable } from "@angular/core";
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
-import { Observable } from "rxjs";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-export class GuardaRotas implements CanActivate {
-    constructor(private router: Router){
-        
-    }
 
-    canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot ): boolean {
-      let autenticado = localStorage.getItem("usuario-autenticado");
-      if(autenticado == "1"){
-          return true;
-      }
-      this.router.navigate(['/entrar'], { queryParams: { returnUrl: state.url } });
-      return false;
-    }
+export class GuardaRotas implements CanActivate {
+  constructor(private router: Router) {
+
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (sessionStorage.getItem("usuario-autenticado") == "1")
+      return true;
+    
+    this.router.navigate(['/entrar'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
 
 }
--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,25 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../model/usuario";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-    selector: "app-login",
-    templateUrl: "./login.component.html",
-    styleUrls: ["./login.component.css", ]
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css",]
 })
 
-export class LoginComponent {
-    public usuario;
+export class LoginComponent implements OnInit {
+  public usuario;
+  public returnUrl: string;
 
-    constructor(private router: Router){
-        this.usuario = new Usuario();
-    }
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {  }
 
-    entrar(){
-        if(this.usuario.email == "" && this.usuario.senha == ""){
-            sessionStorage.setItem("usuario-autenticado", "1");
-            //this.router.navigate(['/']);
-        }
+  ngOnInit(): void {
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
+    this.usuario = new Usuario();
+  }
+
+  entrar() {
+    if (this.usuario.email == "fer@gmail.com" && this.usuario.senha == "123") {
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
     }
+  }
 
 }
